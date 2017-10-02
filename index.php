e4ac6fdb98cebf5292d511eef311e07f91bdf1de
Thomas Szymanski <thomas.szymanski15@gmail.com>
2017-10-02 14:09:01 +0200

    Correction exercice 2 prairie PHP

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 
-  $texte = "La Vie aquatique (The Life Aquatic with Steve Zissou), film américain
+  $text = 'La Vie aquatique (The Life Aquatic with Steve Zissou), film américain
            sorti en 2004, est le quatrième long-métrage réalisé par Wes Anderson. Il est
-           dédié à l'océanographe français Jacques-Yves Cousteau et s'inspire librement de
+           dédié à l\'océanographe français Jacques-Yves Cousteau et s\'inspire librement de
            sa vie sur un ton parodique. Le film est une comédie dramatique qui, sous un
            aspect fantaisiste, aborde des thèmes forts comme la famille, le deuil, la
-           vengeance, l'angoisse de vieillir ou l'échec.";
+           vengeance, l\'angoisse de vieillir ou l\'échec.';
+  $text2 = str_replace('e', '', $text);
+  echo $text2;
 
-  $texte2 = str_replace("e", "", $texte );
-
-  echo $texte2;
-  
  ?>
